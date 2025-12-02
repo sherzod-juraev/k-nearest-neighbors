34df3eed70c61601232d838a2048e975e0c7228a
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-02 14:44:34 +0500

    main file updated

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from modules import modules_router
 
 
 app = FastAPI()
+
+app.include_router(
+    modules_router,
+    prefix='/knn',
+    tags=['K nearest neighbors']
+)
