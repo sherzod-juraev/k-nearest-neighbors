26c0da1dc94436dd0132eaff397277521b03b0ba
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-02 13:14:20 +0500

    scheme file updated in modules package

--- modules/scheme.py
+++ modules/scheme.py
@@ -1,9 +1,76 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, model_validator
+from fastapi import HTTPException, status
+from numpy import array, isnan
+
+
+class KNearestNeighborsIn(BaseModel):
+    model_config = {
+        'extra': 'forbid'
+    }
+
+    X: list[list[float]]
+    y: list[int]
+
+
+    @field_validator('X')
+    def verify_X(cls, value):
+        X = array(value)
+        if X.ndim != 2:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='Must be a 2D matrix'
+            )
+        if isnan(X).any():
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='Matrix X must not contain NaN values'
+            )
+        return X
+
+
+    @field_validator('y')
+    def verify_y(cls, value):
+        y = array(value)
+        if y.ndim != 1:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='Must be a 1D vector'
+            )
+        if isnan(y).any():
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='Vector y must not contain NaN values'
+            )
+        return y
+
+    @model_validator(mode='after')
+    def verify_object(self):
+        if self.X.shape[0] != self.y.shape[0]:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='The number of n_sample and target values must be equal.'
+            )
+        return self
 
 
 class KNearestNeighbors(BaseModel):
-    pass
+    model_config = {
+        'extra': 'forbid'
+    }
 
+    X: list[float]
 
-class KNearestNeighbors(BaseModel):
-    pass
+    @field_validator('X')
+    def verify_X(cls, value):
+        X = array(value)
+        if X.ndim != 1:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='Must be a 1D vector'
+            )
+        if isnan(X).any():
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='Vector X must not contain NaN values'
+            )
+        return X
